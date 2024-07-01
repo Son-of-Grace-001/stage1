a221b7ef624a2b27d6f62898ee917d684a7c417b
Son-of-Grace-001 <horlharmighty2000@gmail.com>
2024-07-01 18:38:23 +0100

    hi

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from . import views
-
+from .views import hello
 
 urlpatterns = [
-  path('stageone', views.stageone),
+    path("hello/<str:visitor_name>", hello, name='hello'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,27 +1,29 @@
-from django.shortcuts import render
-
-# Create your views here.
+import os
 import requests
 from django.http import JsonResponse
 
-def stageone(request):
-    visitor_name = request.GET.get('visitor_name', 'Guest')
+def hello(request, visitor_name):
+    provided_city = request.GET.get('city')
+    api_key = os.getenv('OPENWEATHERMAP_API_KEY')
 
     # Get client IP
     client_ip = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR'))
 
     try:
-        # Get location info
-        location_response = requests.get(f'http://ipinfo.io/{client_ip}/json')
-        location_data = location_response.json()
-        city = location_data.get('city', 'Unknown City')
+        if provided_city:
+            city = provided_city
+        else:
+            # Get location info based on IP
+            location_response = requests.get(f'http://ipinfo.io/{client_ip}/json')
+            location_data = location_response.json()
+            city = location_data.get('city', 'Unknown City')
 
         if city == 'Unknown City':
             raise ValueError('City could not be determined from IP address')
 
         # Get weather info
         weather_response = requests.get(
-            f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=5d283a99d22ce2b152ec51514eb42a88'
+            f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
         )
         weather_data = weather_response.json()
 
